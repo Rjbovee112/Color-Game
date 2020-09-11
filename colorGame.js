b32d0f50ae4f8cf7e4798e1ac2a35b05213693d1
Robert Bovee <Rjbovee112@gmail.com>
2020-09-10 20:13:07 -0600

    working on logic for random colors

--- colorGame.js
+++ colorGame.js
@@ -1,13 +1,6 @@
 alert("CONNECTED!");
 
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickedColor();
@@ -50,4 +43,24 @@ function changeColors(color) {
 function pickedColor() {
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+function generateRandomColors(num){
+    //make an array
+    var aar = []
+    //repeat num times
+    for(var i = 0; i < num; i ++){
+        //get random color and push into arr
+
+    }
+    //then return
+    return arr;
+}
+
+function randomColor(){
+    //pick a "red" from 0-255
+    Math.floor(Math.random() * 256)
+    //pick a "green" form 0-255
+    //pick a "blue" from 0-255
+
 }
