fa8e4838e76ce97d743364c00cd2a2ef2c664703
Robert Bovee <Rjbovee112@gmail.com>
2020-09-13 19:38:31 -0600

    added correct and wrong in a span

--- colorGame.js
+++ colorGame.js
@@ -11,6 +11,7 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +24,10 @@ for (var i = 0; i < squares.length; i++) {
         var clickedColor = this.style.backgroundColor;
         //compare color of pickedColor
         if (clickedColor === pickedColor) {
-            alert("Correct!");
+            messageDisplay.textContent = "Correct!";
         } else {
             this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again"
         }
     });
 }
